f92fd72d77154f9d86bfb5f010d54905b31c6822
Anton Logunov <skykeeper925@gmail.com>
2021-05-21 10:54:27 +0300

    Test part fixing

--- test/todos-test.js
+++ test/todos-test.js
@@ -9,6 +9,11 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+function query (taskText) {
+    let todo = Todo.build({taskText: taskText, isDone: false});
+    return todo.save();
+}
+
 describe('Todos', function () {
 
     beforeEach(async () => {
@@ -19,7 +24,7 @@ describe('Todos', function () {
     });
 
     describe('/GET todo', function () {
-        it('it should GET all todos', (done) => {
+        it('it should GET empty todos array', (done) => {
             chai.request(server)
                 .get('/todos')
                 .end((err, res) => {
@@ -31,21 +36,33 @@ describe('Todos', function () {
         });
     });
 
+    describe('/GET todo', function () {
+        it('it should GET all todos', (done) => {
+            let N =3;
+            for (let i = 1; i <= N; i++) {
+                query("Todo №" + i)
+            }
+            chai.request(server)
+                .get('/todos')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(3);
+                    done();
+                });
+        });
+    });
 
     describe('/POST todo', function () {
-        it('it should not POST a todo without isDone field', (done) => {
+        it('it should not POST a todo without taskText field', (done) => {
             let todo = {
-                taskText: 'Some task'
+                isDone: false
             };
             chai.request(server)
                 .post('/todos')
                 .send(todo)
                 .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('object');
-                    res.body.should.have.property('id');
-                    res.body.should.have.property('taskText').eql('Some task');
-                    res.body.should.have.property('isDone').eql(false);
+                    res.should.have.status(500);
                     done();
                 });
         });
@@ -71,12 +88,11 @@ describe('Todos', function () {
 
     describe('/PATCH/:id todo', function () {
         it('it should UPDATE a todo given the id', (done) => {
-            let todo = Todo.build({taskText: "Task for updating", isDone: false});
-            todo.save().then(result => {
+           query('Task for updating').then(result => {
                 chai.request(server)
                     .patch('/todos/' + result.id)
                     .set('content-type', 'application/json')
-                    .send({isDone: todo.isDone})
+                    .send({isDone: result.isDone})
                     .end((err, res) => {
                         res.should.have.status(200);
                         res.body.should.be.a('object');
@@ -90,9 +106,8 @@ describe('Todos', function () {
     });
 
     describe('/delete', function () {
-        it('it should DELETE a todos', (done) => {
-            let todo = Todo.build({taskText: "Task for delete", isDone: true});
-            todo.save();
+        it('it should DELETE all todos', (done) => {
+            query('Task for deleting')
                 chai.request(server)
                     .delete('/todos')
                     .set('content-type', 'application/json')
@@ -103,4 +118,22 @@ describe('Todos', function () {
                     });
         });
     });
+
+    describe('/delete', function () {
+
+        it('it should delete a todo given the id', (done) => {
+           query('Task for deleting').then(result => {
+                chai.request(server)
+                    .delete('/todos')
+                    .set('content-type', 'application/json')
+                    .send([result.id])
+                    .end((err, res) => {
+                        res.should.have.status(204);
+                        done();
+                    });
+            });
+        });
+
+    });
+
 });
